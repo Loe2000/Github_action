51ed95395ef9b7c627676a57bb5df1f494d08ccb
Loe2000 <loe@oregonstate.edu>
2021-06-06 19:49:15 -0700

    fixed linting errors again

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ class TestCase(unittest.TestCase):
     def test_add_1(self):
         self.assertEqual(example.addition(1, 2), 3)
 
+
     def test_sub_1(self):
         self.assertEqual(example.subtraction(8, 2), 6)
 
