59aaae1795d4d576669a96fe7fac23bcb9fcd1b6
Loe2000 <loe@oregonstate.edu>
2021-06-06 19:52:02 -0700

    added multiplication test/method also fixed more linting errors because im impatient

--- test.py
+++ test.py
@@ -7,9 +7,11 @@ class TestCase(unittest.TestCase):
     def test_add_1(self):
         self.assertEqual(example.addition(1, 2), 3)
 
-
     def test_sub_1(self):
         self.assertEqual(example.subtraction(8, 2), 6)
+
+    def test_mult_1(self):
+        self.assertEqual(example.addition(1, 2), 3)
 
 
 if __name__ == '__main__':
